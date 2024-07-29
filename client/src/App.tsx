c4385e8d69ba0ee01f13dd982b5eaef67bdcd354
Loges <sivalog25@gmail.com>
2024-07-29 18:43:35 +0530

    deploy commit

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,20 +1,21 @@
-import {BrowserRouter as Router,Route,Routes} from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import './App.css'
 import { Dashboard } from './pages/dashboard';
 import { Auth } from './pages/auth';
 import { FinancialRecordsProvider } from './context/financial-record-context';
+import { SignedIn, UserButton } from "@clerk/clerk-react";
 
 function App() {
 
   return (
     <Router>
       <div className="app-container">
-        {/* <div className="navbar">
+         <div className="navbar">
           <Link to="/"> Dashboard</Link>
           <SignedIn>
             <UserButton />
           </SignedIn>
-        </div> */}
+        </div> 
         <Routes>
           <Route path="/" element={
             <FinancialRecordsProvider>
